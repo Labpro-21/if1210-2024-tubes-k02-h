8b24681e4d8d0f4bd8d2683b6bf2507d5c3be536
jovan196 <168165681+jovan196@users.noreply.github.com>
2024-05-14 13:33:04 +0700

    Add files via upload

---
+++ F16_Exit.py
@@ -0,0 +1,19 @@
+def exit_game():
+    from F15_Save import save
+    global list_user
+    global list_monster
+    global list_item_inventory
+    global list_monster_inventory
+    global list_item_shop
+    global list_monster_shop
+    while True:
+        user_input = input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n) ").lower()
+        if user_input == 'y':
+            # Menjalankan prosedur save
+            save()
+            save(list_user, list_monster, list_item_inventory, list_monster_inventory, list_item_shop, list_monster_shop)
+        elif user_input == 'n':
+            # Keluar program
+            exit()
+        else:
+            continue
