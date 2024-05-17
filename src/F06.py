b92251acf34888ebd74d8cbbc5c08fa9871556d4
jovan196 <168165681+jovan196@users.noreply.github.com>
2024-05-17 15:30:37 +0700

    Update F06.py

--- src/F06.py
+++ src/F06.py
@@ -1,3 +1,9 @@
+def mini(a,b):
+    if a < b:
+        return a
+    else:
+        return b
+    
 def potion(selected_potion_name, selected_user_monster, login_id, potion_inventory,monster_data):
     for potion in potion_inventory:
         if potion['user_id'] == login_id and potion['type'] == selected_potion_name:
@@ -10,9 +16,9 @@ def potion(selected_potion_name, selected_user_monster, login_id, potion_invento
                     potion['quantity'] = str(int(potion['quantity']) - 1)
                 elif selected_potion_name.lower() == 'healing':
                     level = int(selected_user_monster['level'])
-                    factor = 1 + (int(level) - 1) * 0.1
-                    monster = [m for m in monster_data if selected_user_monster['type'] == m['type']][0]
-                    selected_user_monster['hp'] = str(int(int(selected_user_monster['hp']) * 1.25))
+                    factor = 1 + (int(level) - 1) * 0.1 # Kapasitas maksimum HP monster dari CSV dikali faktor berdasarkan levelnya
+                    monster = [m for m in monster_data if selected_user_monster['id'] == m['id']][0]
+                    selected_user_monster['hp'] = str(mini(int(int(selected_user_monster['hp']) * 1.25),int(int(monster["hp"]) * factor)))
                     potion['quantity'] = str(int(potion['quantity']) - 1)
             else:
                 print("Potion habis")
