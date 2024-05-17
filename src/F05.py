abdc1c7e563dc4fa9b2038f7ecf77bce6104027f
jovan196 <168165681+jovan196@users.noreply.github.com>
2024-05-17 23:29:20 +0700

    Update F05.py

--- src/F05.py
+++ src/F05.py
@@ -30,3 +30,13 @@ def attack(attacker, defender):
     damage = int(attacker_atk_power * (1 - defender_def_power / 100))
     defender['hp'] = maxi(int(defender['hp']) - maxi(damage, 0), 0)  # Ensure damage is non-negative
     return attacker,defender
+
+def attack_arena(attacker, defender): # Fungsi attack untuk F09
+    from src.F00 import random_num
+    n = random_num(-30, 30)  # Randomize n from -3 to 3
+    atk_multiplier = 1 + n / 100
+    attacker_atk_power = int(attacker['atk_power']) * atk_multiplier
+    defender_def_power = mini(int(defender['def_power']), 50)  # Ensure def power does not exceed 50
+    damage = int(attacker_atk_power * (1 - defender_def_power / 100))
+    defender['hp'] = maxi(int(defender['hp']) - maxi(damage, 0), 0)  # Ensure damage is non-negative
+    return attacker,defender,damage
