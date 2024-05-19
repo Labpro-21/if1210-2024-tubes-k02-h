b5775a7c8acb718eba7afa6202cdc0859022dd38
jovan196 <168165681+jovan196@users.noreply.github.com>
2024-05-19 08:14:08 +0700

    Update F02.py

--- src/F02.py
+++ src/F02.py
@@ -1,23 +1,20 @@
-from all_function import *
-
-with open('user.csv', mode ='r') as file_user:
-    user = read(file_user) 
-
-def login(data):
+def login(list_user):
     username = input("Masukkan username: ")
-    for i in range (1, len (data)):
-        if username == data[i][1]:
+    for i in range (1, len (list_user)):
+        if username == list_user[i][1]:
             password = input("Masukkan password: ")
-            if password == data[i][2]:
-                print(f"Selamat datang {data[i][3]} {data[i][1]}")
-                user_id = data[i][0]
+            if password == list_user[i][2]:
+                print(f"Selamat datang {list_user[i][3]} {list_user[i][1]}")
+                user_id = list_user[i][0]
                 return user_id
             else:
                 print("Password salah")
+                break
         else:
             print("Username tidak terdaftar")
+            break
 
 # Aplikasi pada main.py
 
 # from src.F02 import *
-# login_id = login(user) # login_id dipakai sebagai variabel global di main.py untuk menentukan kepemilikan item, akses fungsi tertentu, dan status login user          
+# login_id = login(li_user) # login_id dipakai sebagai variabel global di main.py untuk menentukan kepemilikan item, akses fungsi tertentu, dan status login user          
