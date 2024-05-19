34485025b14b95be3e8aa89cd46e4b1b7c3c2be2
jovan196 <168165681+jovan196@users.noreply.github.com>
2024-05-19 08:38:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from src.F00 import random_num
 from src.F01 import register
 from src.F02 import login
 from src.F03 import logout
@@ -7,36 +6,34 @@ from src.F07 import inventory
 from src.F08 import battle
 from src.F09 import arena
 from src.F10 import shop
-from src.F11 import laboratory
 from src.F12 import shop_management
-from src.F13 import monster_management
 from src.F14 import load
 from src.F15 import save
 from src.F16 import exit_game
 
 li_user, li_monster, li_item, li_item_inventory, li_monster_inventory, li_item_shop, li_monster_shop = load()
-login_id = "12345" # Asumsi user login
+login_id = None # Asumsi user login
 
 while True:
-    if login_id:
-        while True:
-            a = input("inventory/battle/arena/shop/save/quit: ").lower()
-            if a == 'inventory':
-              inventory(login_id,li_user,li_monster,li_item_inventory,li_monster_inventory)
-            elif a == 'battle':
-              li_user,li_monster,li_item_inventory,li_monster_inventory = battle(login_id,li_user,li_monster,li_item_inventory,li_monster_inventory)
-            elif a == 'arena':
-              li_user,li_monster,li_item_inventory,li_monster_inventory = arena(login_id,li_user,li_monster,li_item_inventory,li_monster_inventory)
-            elif a == 'shop':
-              li_user,li_monster,li_item_inventory,li_monster_inventory,li_item_shop,li_monster_shop = shop(login_id,li_user,li_monster,li_item_inventory,li_monster_inventory,li_item_shop,li_monster_shop)
-            elif a == 'save':
-              save(li_user, li_monster, li_item_inventory, li_monster_inventory, li_item_shop, li_monster_shop)
-            elif a == 'quit':
-              exit_game(li_user, li_monster, li_item_inventory, li_monster_inventory, li_item_shop, li_monster_shop)
-            else:
-              continue
-        break
+  a = input("register/login/logout/help/inventory/battle/arena/shop/lab/shop_mgmt/monster_mgmt/save/quit: ").lower()
+  if a == 'register':
+    li_user,li_monster_inventory = register(li_user,li_monster,li_monster_inventory)
+  elif a == 'login':
+    login_id = str(login(li_user))
+  elif a == 'logout':
+    login_id = logout(login_id)
+  elif a == 'inventory':
+    inventory(login_id,li_user,li_monster,li_item_inventory,li_monster_inventory)
+  elif a == 'battle':
+    li_user,li_monster,li_item_inventory,li_monster_inventory = battle(login_id,li_user,li_monster,li_item_inventory,li_monster_inventory)
+  elif a == 'arena':
+    li_user,li_monster,li_item_inventory,li_monster_inventory = arena(login_id,li_user,li_monster,li_item_inventory,li_monster_inventory)
+  elif a == 'shop':
+    li_user,li_monster,li_item_inventory,li_monster_inventory,li_item_shop,li_monster_shop = shop(login_id,li_user,li_monster,li_item_inventory,li_monster_inventory,li_item_shop,li_monster_shop)
+  elif a == 'save':
+    save(li_user, li_monster, li_item_inventory, li_monster_inventory, li_item_shop, li_monster_shop)
+  elif a == 'quit':
+    exit_game(li_user, li_monster, li_item_inventory, li_monster_inventory, li_item_shop, li_monster_shop)
+  else:
+    continue
 
-    else:
-        print("Silakan login dahulu")
-        break
