c73a5c7469ff358dc9138dc72df9276d9b95529c
jovan196 <168165681+jovan196@users.noreply.github.com>
2024-05-19 23:40:54 +0700

    Delete logout.py

--- logout.py
+++
@@ -1,7 +0,0 @@
-def logout(user):
-    if user['is_logged_in']:
-        user['is_logged_in'] = False
-        print(f"Pengguna {user['username']} telah logout.")
-        user['username'] = None
-    else:
-        print("Logout gagal! Anda belum login, silahkan login terlebih dahulu sebelum melakukan logout.")
