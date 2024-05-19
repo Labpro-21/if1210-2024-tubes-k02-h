eeb3af5b843673bca3b651165e08591041d5014b
jovan196 <168165681+jovan196@users.noreply.github.com>
2024-05-19 08:15:47 +0700

    Update F05.py

--- src/F05.py
+++ src/F05.py
@@ -9,6 +9,23 @@ def mini(a,b):
         return a
     else:
         return b
+
+def custom_zip(*args):
+    def zipper(**attr):
+        return attr
+    iterators = [iter(iterable) for iterable in args]
+    sentinel = zipper()
+    
+    while True:
+        result = tuple(next(iterator, sentinel) for iterator in iterators)
+        if any(val is sentinel for val in result):
+            return
+        yield result
+
+def custom_isdigit(s):
+    if isinstance(s, int):
+        return True
+    return all('0' <= char <= '9' for char in str(s))
     
 def adjust(monster):
     level = int(monster['level'])
@@ -21,7 +38,7 @@ def adjust(monster):
         monster['hp'] = int(int(monster['hp']) * factor)
         return monster
 
-def attack(attacker, defender):
+def attack(attacker, defender): # Fungsi attack untuk F08
     from src.F00 import random_num
     n = random_num(-30, 30)  # Randomize n from -3 to 3
     atk_multiplier = 1 + n / 100
